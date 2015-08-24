b72685577d9a885b7257b1d301e36dfd0a957df2
mdelura <deluramichal@gmail.com>
2015-08-24 22:50:42 +0200

    Add CursorAdapter

--- app/src/main/java/com/gmail/deluramichal/aukletnewsreader/MainActivityFragment.java
+++ app/src/main/java/com/gmail/deluramichal/aukletnewsreader/MainActivityFragment.java
@@ -1,16 +1,60 @@
 package com.gmail.deluramichal.aukletnewsreader;
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.gmail.deluramichal.aukletnewsreader.data.NewsContract;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private final static String URL_TVN24 = "http://www.tvn24.pl/najnowsze.xml";
+    private final static String URL_REUTERS = "http://feeds.reuters.com/news/artsculture";
+    private final static String URL_WIKIPEDIA = "http://en.wikipedia.org/";
+    private final static String NEWS_MIME_TYPE = "text/html; charset=utf-8";
+    private final static String DEBUG_TAG = MainActivity.class.getSimpleName() + " DEBUG: ";
+    private final static boolean USE_IMAGE = true; //TODO: Get from Settings or something
+    private static final String ACTIVE_ITEM = "active_item";
+    private static final int NEWS_LOADER = 0;
+    private static final String[] NEWS_COLUMNS = {
+            NewsContract.ItemEntry._ID,
+            NewsContract.ItemEntry.COLUMN_TITLE,
+            NewsContract.ItemEntry.COLUMN_LINK,
+            NewsContract.ItemEntry.COLUMN_CONTENT,
+            NewsContract.ItemEntry.COLUMN_DESCRIPTION,
+            NewsContract.ItemEntry.COLUMN_PUB_DATE,
+            NewsContract.ItemEntry.COLUMN_IMAGE_SRC,
+            NewsContract.ItemEntry.COLUMN_IMAGE,
+            NewsContract.ItemEntry.COLUMN_CHANNEL_KEY,
+            NewsContract.ItemEntry.COLUMN_SYNC_DATE};
+
+    //Indices for NEWS_COLUMNS
+    static final int COL_NEWS_ID = 0;
+    static final int COL_TITLE = 1;
+    static final int COL_LINK = 2;
+    static final int COL_CONTENT = 3;
+    static final int COL_DESCRIPTION = 4;
+    static final int COL_PUB_DATE = 5;
+    static final int COL_IMAGE_SRC = 6;
+    static final int COL_IMAGE = 7;
+    static final int COL_CHANNEL_KEY = 8;
+    static final int COL_SYNC_DATE = 9;
+
+    private NewsAdapter mNewsAdapter;//TODO: Implement adapter
+    private int mPosition;
 
     public MainActivityFragment() {
     }
@@ -19,9 +63,85 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View rootView =  inflater.inflate(R.layout.list_view, container, false);
+        
+        //Initialize CursorAdapter
+        mNewsAdapter = new NewsAdapter(getActivity(), null, 0);
+        mNewsAdapter.setUseImage(USE_IMAGE);
+        // Get a reference to the ListView, and attach this adapter to it.
+        ListView listView = (ListView) rootView.findViewById(R.id.list_view);
+        listView.setAdapter(mNewsAdapter);
+
+        if (null !=savedInstanceState && savedInstanceState.containsKey(ACTIVE_ITEM)) {
+            mPosition = savedInstanceState.getInt(ACTIVE_ITEM);
+        }
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // CursorAdapter returns a cursor at the correct position for getItem(), or null
+                // if it cannot seek to that position.
+                Cursor cursor = (Cursor) adapterView.getItemAtPosition(position);
+                if (cursor != null) {
+                    //TODO: *5 Intent to go to link
+                }
+            }});    
+        
+        //TODO: *3 Add Select channels function, see:
+        //http://developer.android.com/training/search/index.html
+        //http://developer.android.com/guide/topics/search/search-dialog.html
+        //TODO: *4 Try it out!
+        //TODO: *6 Add appwidget
+        //TODO: *7 Add Settings
+        //TODO: 8 Select items visible
 
         return rootView;
 
 
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        if (mPosition!=ListView.INVALID_POSITION){
+            outState.putInt(ACTIVE_ITEM, mPosition);
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        getLoaderManager().initLoader(NEWS_LOADER, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+
+        // Sort order:  Descending, by pub_date.
+        String sortOrder = NewsContract.ItemEntry.COLUMN_PUB_DATE + " DESC";
+        Uri newsUri = NewsContract.ItemEntry.buildItemUri(i);//TODO: WTF is id?
+
+        return new CursorLoader(getActivity(),
+                newsUri,
+                NEWS_COLUMNS,
+                null,
+                null,
+                sortOrder);
+    }
+
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        mNewsAdapter.swapCursor(cursor);
+        ListView listView = (ListView) getActivity().findViewById(R.id.list_view);
+        if (mPosition != ListView.INVALID_POSITION) {
+            listView.smoothScrollToPosition(mPosition);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+        mNewsAdapter.swapCursor(null);
     }
 }
---
+++ app/src/main/java/com/gmail/deluramichal/aukletnewsreader/NewsAdapter.java
@@ -0,0 +1,111 @@
+package com.gmail.deluramichal.aukletnewsreader;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Date;
+
+/**
+ * Created by Michal Delura on 2015-08-23.
+ */
+public class NewsAdapter extends CursorAdapter {
+
+    private boolean mUseImage;
+    private static final int VIEW_TYPE_WITH_IMAGE = 0;
+    private static final int VIEW_TYPE_NO_IMAGE = 1;
+
+    public NewsAdapter(Context context, Cursor c, int flags) {
+        super(context, c, flags);
+    }
+
+    public void setUseImage(boolean useImage) {
+        mUseImage = useImage;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        //return (mUseImage && getCursor().getBlob(MainActivityFragment.COL_IMAGE).length!=0) ?
+        return (mUseImage) ?//TODO: Check or implement other
+                VIEW_TYPE_WITH_IMAGE : VIEW_TYPE_NO_IMAGE;
+    }
+
+
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
+        if (viewType == VIEW_TYPE_WITH_IMAGE) {
+            layoutId = R.layout.list_item_with_image;
+        } else if (viewType==VIEW_TYPE_NO_IMAGE) {
+            layoutId = R.layout.list_item_no_image;
+        }
+        View view = LayoutInflater.from(context).inflate(layoutId, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        view.setTag(viewHolder);
+        return view;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+        ViewHolder viewHolder = (ViewHolder) view.getTag();
+        // Read news image ID from cursor
+        viewHolder.newsId = cursor.getInt(MainActivityFragment.COL_NEWS_ID);
+        viewHolder.channelId = cursor.getInt(MainActivityFragment.COL_CHANNEL_KEY);
+        viewHolder.newsLink = cursor.getString(MainActivityFragment.COL_LINK);
+
+        //Set title
+        String title = cursor.getString(MainActivityFragment.COL_TITLE);
+        viewHolder.titleView.setText(title);
+
+        //Set image//TODO: Check if zero-length byte array or something
+        if (getItemViewType(cursor.getPosition())==VIEW_TYPE_WITH_IMAGE) {
+            byte[] imageBytes = cursor.getBlob(MainActivityFragment.COL_IMAGE);
+            Bitmap bitmapFromBytes =
+                    BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+            viewHolder.thumbnailView.setImageBitmap(bitmapFromBytes);
+            viewHolder.thumbnailView.setContentDescription(title);
+        }
+
+        // Read date from cursor
+        long dateInMillis = cursor.getLong(MainActivityFragment.COL_PUB_DATE);
+        viewHolder.dateView.setText(
+                new Date(dateInMillis).toString());
+        viewHolder.descriptionView.setText(cursor.getString(MainActivityFragment.COL_DESCRIPTION));
+
+    }
+
+    /**
+     * Cache of the children views for a news list item.
+     */
+    public static class ViewHolder {
+        public int newsId;
+        public String newsLink;
+        public int channelId;
+        public final ImageView thumbnailView;
+        public final TextView titleView;
+        public final TextView dateView;
+        public final TextView descriptionView;
+
+        public ViewHolder(View view) {
+            thumbnailView = (ImageView) view.findViewById(R.id.list_item_thumbnail);
+            titleView = (TextView) view.findViewById(R.id.list_item_title);
+            dateView = (TextView) view.findViewById(R.id.list_item_date);
+            descriptionView = (TextView) view.findViewById(R.id.list_item_description);
+        }
+    }
+}
